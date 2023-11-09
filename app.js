59db839cf482931d957a539253544c4c39836647
Ripty-Dx <riptyfullstack.dx@gmail.com>
2023-11-09 17:26:28 +0530

    level-2 authentication

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
-//jshint esversion:6
+require("dotenv").config();
 const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// to connect database
-mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
-const userSchema = {
+//step 1:  to connect database
+mongoose
+  .connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true })
+  .then(() => console.log("database connected"))
+  .catch((err) => console.log(err));
+
+// Step:2 to create schema i.e structure of document
+const userSchema = new mongoose.Schema({
   email: String,
   password: String,
-};
+});
+
+// Security
+// optional step
+// encryption for passwords in database storage
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+
+//step:3 model creation: an interface for CRUD operations
 const User = new mongoose.model("User", userSchema);
 
 app.get("/", (req, res) => {
@@ -24,15 +37,12 @@ app.get("/", (req, res) => {
 app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
+
 app.post("/register", (req, res) => {
   const newUser = new User({
     email: req.body.username,
     password: req.body.password,
   });
-  //   newUser.save((err) => {
-  //     if (err) console.log(err);
-  //     else res.render("secrets.ejs");
-  //   });
   newUser
     .save()
     .then(() => {
